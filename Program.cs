a0d1b73c958b679f1d04f839617bd95517817394
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 01:25:36 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double km, milhas;
+
+Console.WriteLine("digite as milhas por favor:");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+km = milhas * 1.609;
+
+Console.WriteLine($"a medida em quilometros é: {km:N3} km");
